38a993a4c15405e6746ebc821f55a7b761726d36
salas <vladyslav.cartel88@gmail.com>
2021-03-15 22:32:10 +0200

    liquibase add properties and app started without exseptions

---
+++ src/main/java/net/proselate/jwtappdemo/JwtappdemoApplication.java
@@ -0,0 +1,13 @@
+package net.proselate.jwtappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtappdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtappdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/proselate/jwtappdemo/JwtappdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.proselate.jwtappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtappdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
